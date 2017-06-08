7223fa7d4d3ffb0d347669003672b40e26300101
panx27 <panx27@gmail.com>
2017-06-08 13:07:18 -0400

    add compare_tabs.py

--- scripts/trim_tab.py
+++ scripts/trim_tab.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 import re
 import sys
 import logging
